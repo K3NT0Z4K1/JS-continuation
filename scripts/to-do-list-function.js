4be7346ee87079ad5d15f0f45dd83f9686cefd5c
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-01-16 18:51:54 +0800

    added add function

--- scripts/to-do-list-function.js
+++ scripts/to-do-list-function.js
@@ -2,10 +2,20 @@ const toDoArray = [];
 
 function addToArray(){
 
-  document.querySelector()
+  const toDoInputElem = document.querySelector('.js-toDo-input').value;
 
-  toDoArray.push()
 
+  toDoArray.push(toDoInputElem);
+
+  console.log(toDoArray);
+
+
+
+}
+
+function displayToDo(){
+
+  //  document.querySelector('.js-input-display').innerHTML =
 
 
 }
