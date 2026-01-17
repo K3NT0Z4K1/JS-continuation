7154a11dfcd40943c017ebff4b186d5f23b605f7
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-01-17 13:10:38 +0800

    Tried

--- lesson 11 exercise/11.js
+++ lesson 11 exercise/11.js
@@ -9,21 +9,36 @@ nums[2] = 99;
 console.log(nums);
 */
 
-// 11b
+
+
 
 const nums = [10,20,30,40,50];
 const animals = ['iro', 'iring', 'tawo'];
 
-
+// 11b function
 function getLastValue(array){
 
 const lastValue = array.length-1;
 
 console.log(array[lastValue]);
+}
+
+
+// 11c function
+function arraySwap(array){
+
+  const firstValue = array[0];
+
+  const tobeLast = firstValue;
+
+  const lastValueIndex = array.length - 1;
+
+  const lastValue = array[lastValueIndex];
+
+  firstValue = lastValue;
+
+  lastValue = tobeLast;
 
 
 }
 
-getLastValue(nums);
-
-getLastValue(animals);
