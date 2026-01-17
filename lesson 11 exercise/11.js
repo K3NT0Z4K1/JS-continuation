747c2f7a74fa84f133ad887b24803a6d8e43b4c3
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-01-17 13:05:12 +0800

    11b done

--- lesson 11 exercise/11.js
+++ lesson 11 exercise/11.js
@@ -1,5 +1,29 @@
+
+// 11a
+
+/*
 const nums = [10,20,30];
 
 nums[2] = 99;
 
 console.log(nums);
+*/
+
+// 11b
+
+const nums = [10,20,30,40,50];
+const animals = ['iro', 'iring', 'tawo'];
+
+
+function getLastValue(array){
+
+const lastValue = array.length-1;
+
+console.log(array[lastValue]);
+
+
+}
+
+getLastValue(nums);
+
+getLastValue(animals);
