fa1560f9dab6571d4a06e6fde0b04e6b48682ff3
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-02-01 11:49:50 +0800

    8b done

---
+++ lesson 8 exercise/8.js
@@ -0,0 +1,20 @@
+
+// 8a
+
+const product = {
+
+  name: 'basketball',
+  price: '2095 cents'
+
+}
+
+console.log(product);
+
+
+// 8b
+
+product.price = '500 cents';
+
+console.log(`Updated Price ${product.price}`);
+
+console.log(product);
