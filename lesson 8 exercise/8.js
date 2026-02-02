647ee2ff8ed1c3cb755b8d99abd7e752f8d8bce7
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-02-02 20:45:39 +0800

    Renamed the function

--- lesson 8 exercise/8.js
+++ lesson 8 exercise/8.js
@@ -64,7 +64,7 @@ function comparePrice(product1, product2){
 
 // 8e
 
-function isSameProduction(product1,product2){
+function isSameProduct(product1,product2){
 
   if(product1.name === product2.name && product1.price === product2.price){
 
@@ -74,5 +74,6 @@ function isSameProduction(product1,product2){
   }
 
 }
+
 
 
