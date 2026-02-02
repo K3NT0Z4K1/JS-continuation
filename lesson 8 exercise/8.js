7e3ab493da87577431c777cb1ee753a0d3cb2197
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-02-02 20:42:11 +0800

    Added if else in it

--- lesson 8 exercise/8.js
+++ lesson 8 exercise/8.js
@@ -66,6 +66,13 @@ function comparePrice(product1, product2){
 
 function isSameProduction(product1,product2){
 
+  if(product1.name === product2.name && product1.price === product2.price){
+
+    return true;
+  }else{
+    return false;
+  }
+
 }
 
 
